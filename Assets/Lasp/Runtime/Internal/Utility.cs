e915069525bc1794ae20efd53d008a3ead3c466c
Keijiro Takahashi <keijiro@gmail.com>
2020-03-05 14:46:04 +0900

    Cosmetic changes

--- Assets/Lasp/Runtime/Internal/Utility.cs
+++ Assets/Lasp/Runtime/Internal/Utility.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
 
-// Extension methods for NativeArray <-> ReadOnlySpan conversion
-
 namespace Lasp
 {
+    // Extension methods for converting NativeArray <-> ReadOnlySpan
     static class SpanNativeArrayExtensions
     {
+        // ReadOnlySpan -> NativeArray
         public unsafe static NativeArray<T>
           GetNativeArray<T>(this ReadOnlySpan<T> span) where T : unmanaged
         {
@@ -27,6 +27,7 @@ namespace Lasp
             }
         }
 
+        // NativeArray -> ReadOnlySpan
         public unsafe static ReadOnlySpan<T>
           GetReadOnlySpan<T>(this NativeArray<T> array) where T : unmanaged
             => new Span<T>
@@ -34,8 +35,10 @@ namespace Lasp
                array.Length);
     }
 
+    // Extension methods for List<T>
     static class ListExtensions
     {
+        // Find and retrieve an entry with removing it
         public static T FindAndRemove<T>(this List<T> list, Predicate<T> match)
         {
             var index = list.FindIndex(match);
